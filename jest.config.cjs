081ca987ce4e8dd19f78b8f7247747be471c1f83
lski <lee.cooper@lski.uk>
2021-09-25 13:17:30 +0100

    Extract jest config from package.json

---
+++ jest.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/mocks/fileMock.ts',
+		'\\.(css|less|scss)$': 'identity-obj-proxy',
+	},
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+	testEnvironment: 'jsdom',
+};
